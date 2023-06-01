c8a0a6f1efd2c4cf9d3709c3f46a26458ef728d4
zc <syfhub@hotmail.com>
2023-06-01 22:19:10 +0800

    initial commit

--- noxfile.py
+++ noxfile.py
@@ -7,7 +7,6 @@ import nox
 @nox.session(reuse_venv=True)
 def docs(session):
     session.install("-r", "docs/requirements.txt")
-    session.install(".")
 
     # Generate documentation into `build/docs`
     session.run("sphinx-build", "-b", "dirhtml", "-v", "docs/", "build/docs")
